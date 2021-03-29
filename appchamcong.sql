139c688ede19ad4bd5e9eef73d7b86f30c9826f2
ThanhTung <63633930+mrtung1999hpvn@users.noreply.github.com>
2021-03-29 07:32:50 +0700

    Add files via upload

--- appchamcong.sql
+++ appchamcong.sql
@@ -2,8 +2,8 @@
 -- version 5.1.0
 -- https://www.phpmyadmin.net/
 --
--- Máy chủ: 127.0.0.1
--- Thời gian đã tạo: Th3 26, 2021 lúc 07:19 AM
+-- Máy chủ: 127.0.0.1:3306
+-- Thời gian đã tạo: Th3 29, 2021 lúc 02:32 AM
 -- Phiên bản máy phục vụ: 10.4.18-MariaDB
 -- Phiên bản PHP: 8.0.3
 
@@ -20,6 +20,78 @@ SET time_zone = "+00:00";
 --
 -- Cơ sở dữ liệu: `appchamcong`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `tbl_cong_nhan_vien`
+--
+
+CREATE TABLE `tbl_cong_nhan_vien` (
+  `id_cong_nhan_vien` bigint(20) UNSIGNED NOT NULL,
+  `id_cong_nhan` bigint(20) NOT NULL,
+  `ngay_thuc_hien` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Đang đổ dữ liệu cho bảng `tbl_cong_nhan_vien`
+--
+
+INSERT INTO `tbl_cong_nhan_vien` (`id_cong_nhan_vien`, `id_cong_nhan`, `ngay_thuc_hien`) VALUES
+(1, 2, '2021-03-29'),
+(2, 3, '2021-03-30'),
+(10, 4, '2021-03-31'),
+(11, 3, '2021-03-27');
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `tbl_cong_nhan_vien_chi_tiet`
+--
+
+CREATE TABLE `tbl_cong_nhan_vien_chi_tiet` (
+  `id_cong_nhan_vien_chi_tiet` bigint(20) UNSIGNED NOT NULL,
+  `gio_ra_cong` datetime NOT NULL,
+  `gio_vao_thuc_te` datetime NOT NULL,
+  `gio_ra_thuc_te` datetime NOT NULL,
+  `ngay_lam_viec` date NOT NULL,
+  `id_cong_nhan_vien` bigint(20) NOT NULL,
+  `gio_vao_cong` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Đang đổ dữ liệu cho bảng `tbl_cong_nhan_vien_chi_tiet`
+--
+
+INSERT INTO `tbl_cong_nhan_vien_chi_tiet` (`id_cong_nhan_vien_chi_tiet`, `gio_ra_cong`, `gio_vao_thuc_te`, `gio_ra_thuc_te`, `ngay_lam_viec`, `id_cong_nhan_vien`, `gio_vao_cong`) VALUES
+(3, '2021-03-29 00:00:00', '0000-00-00 00:00:00', '0000-00-00 00:00:00', '2021-03-29', 1, NULL),
+(4, '2021-03-30 16:00:00', '0000-00-00 00:00:00', '0000-00-00 00:00:00', '2021-03-30', 2, NULL),
+(5, '2021-03-31 16:00:00', '0000-00-00 00:00:00', '0000-00-00 00:00:00', '2021-03-31', 10, NULL),
+(6, '2021-03-27 16:00:00', '2021-03-27 08:00:00', '0000-00-00 00:00:00', '2021-03-27', 11, '2021-03-27 08:00:00');
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `tbl_du_an`
+--
+
+CREATE TABLE `tbl_du_an` (
+  `id_du_an` bigint(20) UNSIGNED NOT NULL,
+  `ten_du_an` varchar(200) NOT NULL,
+  `noi_dung` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `tbl_du_an_chi_tiet`
+--
+
+CREATE TABLE `tbl_du_an_chi_tiet` (
+  `id_du_an_chi_tiet` bigint(20) UNSIGNED NOT NULL,
+  `id_du_an` bigint(20) NOT NULL,
+  `id_nhan_vien` bigint(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -63,11 +135,14 @@ CREATE TABLE `tbl_nguoi_dung` (
 INSERT INTO `tbl_nguoi_dung` (`id_nguoi_dung`, `ten_dang_nhap`, `mat_khau`, `id_nhan_vien`, `trang_thai`) VALUES
 (1, 'admin@gmail.com', 'test', 1, 0),
 (2, 'test@gmail.com', 'matkhau', 1, 0),
-(3, 'test1@gmail.com', 'admin', 3, 1),
 (4, 'test1@gmail.com', 'admin', 3, 0),
 (5, 'test3@gmail.com', 'admin', 2, 0),
 (6, 'nguyenvanB@gmail.com', 'admin123', 2, 0),
-(7, 'nguyenvanB@gmail.com', 'admin123', 2, 0);
+(7, 'nguyenvanB@gmail.com', 'admin123', 2, 0),
+(8, 'undefined', 'undefined', 1, 0),
+(9, 'undefined', 'undefined', 1, 0),
+(10, 'undefined', 'undefined', 1, 0),
+(11, 'undefined', 'undefined', 1, 0);
 
 -- --------------------------------------------------------
 
@@ -89,7 +164,32 @@ CREATE TABLE `tbl_nhan_vien` (
 
 INSERT INTO `tbl_nhan_vien` (`id_loai_nhan_vien`, `id_nhan_vien`, `ten_nhan_vien`, `lien_he`, `noi_quy`) VALUES
 (0, 0, '[value-3]', '[value-4]', '[value-5]'),
-(3, 2, 'Nguyễn Văn B', '123456789', 'Thực hiện nội quy');
+(3, 2, '', '', '');
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `tbl_thong_bao`
+--
+
+CREATE TABLE `tbl_thong_bao` (
+  `id_thong_bao` bigint(20) UNSIGNED NOT NULL,
+  `ten_thong_bao` varchar(200) NOT NULL,
+  `noi_dung` varchar(500) NOT NULL,
+  `thoi_gian` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `tbl_thong_bao_chi_tiet`
+--
+
+CREATE TABLE `tbl_thong_bao_chi_tiet` (
+  `id_thong_bao_chi_tiet` datetime NOT NULL,
+  `id_thong_bao` bigint(20) NOT NULL,
+  `id_nhan_vien` bigint(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -115,6 +215,31 @@ INSERT INTO `test` (`id`, `ten`) VALUES
 --
 
 --
+-- Chỉ mục cho bảng `tbl_cong_nhan_vien`
+--
+ALTER TABLE `tbl_cong_nhan_vien`
+  ADD PRIMARY KEY (`id_cong_nhan_vien`),
+  ADD UNIQUE KEY `id_cong_nhan_vien` (`id_cong_nhan_vien`);
+
+--
+-- Chỉ mục cho bảng `tbl_cong_nhan_vien_chi_tiet`
+--
+ALTER TABLE `tbl_cong_nhan_vien_chi_tiet`
+  ADD UNIQUE KEY `id_cong_nhan_vien_chi_tiet` (`id_cong_nhan_vien_chi_tiet`);
+
+--
+-- Chỉ mục cho bảng `tbl_du_an`
+--
+ALTER TABLE `tbl_du_an`
+  ADD UNIQUE KEY `tbl_du_an` (`id_du_an`);
+
+--
+-- Chỉ mục cho bảng `tbl_du_an_chi_tiet`
+--
+ALTER TABLE `tbl_du_an_chi_tiet`
+  ADD UNIQUE KEY `id_cong_viec_chi_tiet` (`id_du_an_chi_tiet`);
+
+--
 -- Chỉ mục cho bảng `tbl_loai_nhan_vien`
 --
 ALTER TABLE `tbl_loai_nhan_vien`
@@ -136,6 +261,19 @@ ALTER TABLE `tbl_nhan_vien`
   ADD UNIQUE KEY `id_nhan_vien` (`id_nhan_vien`);
 
 --
+-- Chỉ mục cho bảng `tbl_thong_bao`
+--
+ALTER TABLE `tbl_thong_bao`
+  ADD PRIMARY KEY (`id_thong_bao`),
+  ADD UNIQUE KEY `id_thong_bao` (`id_thong_bao`);
+
+--
+-- Chỉ mục cho bảng `tbl_thong_bao_chi_tiet`
+--
+ALTER TABLE `tbl_thong_bao_chi_tiet`
+  ADD PRIMARY KEY (`id_thong_bao_chi_tiet`);
+
+--
 -- Chỉ mục cho bảng `test`
 --
 ALTER TABLE `test`
@@ -144,6 +282,30 @@ ALTER TABLE `test`
 --
 -- AUTO_INCREMENT cho các bảng đã đổ
 --
+
+--
+-- AUTO_INCREMENT cho bảng `tbl_cong_nhan_vien`
+--
+ALTER TABLE `tbl_cong_nhan_vien`
+  MODIFY `id_cong_nhan_vien` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- AUTO_INCREMENT cho bảng `tbl_cong_nhan_vien_chi_tiet`
+--
+ALTER TABLE `tbl_cong_nhan_vien_chi_tiet`
+  MODIFY `id_cong_nhan_vien_chi_tiet` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT cho bảng `tbl_du_an`
+--
+ALTER TABLE `tbl_du_an`
+  MODIFY `id_du_an` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT cho bảng `tbl_du_an_chi_tiet`
+--
+ALTER TABLE `tbl_du_an_chi_tiet`
+  MODIFY `id_du_an_chi_tiet` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT cho bảng `tbl_loai_nhan_vien`
@@ -155,13 +317,19 @@ ALTER TABLE `tbl_loai_nhan_vien`
 -- AUTO_INCREMENT cho bảng `tbl_nguoi_dung`
 --
 ALTER TABLE `tbl_nguoi_dung`
-  MODIFY `id_nguoi_dung` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id_nguoi_dung` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT cho bảng `tbl_nhan_vien`
 --
 ALTER TABLE `tbl_nhan_vien`
   MODIFY `id_nhan_vien` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT cho bảng `tbl_thong_bao`
+--
+ALTER TABLE `tbl_thong_bao`
+  MODIFY `id_thong_bao` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT cho bảng `test`
--- index.js
+++ index.js
@@ -4,14 +4,15 @@
 const http = require("http");
 /** @format */
 const express = require("express");
-
+var bodyParser = require("body-parser");
 const cors = require("cors");
 const app = express();
 const port = 3001;
 app.use(cors());
 app.use(express.json());
 const pool = require("./mysqlconnect");
-
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 // API
 
 //#region (1) Tổng quan hệ thống
@@ -34,24 +35,25 @@ app.get("/Test", async (req, res) => {
   }
 });
 
-
 app.get("/DangNhap/:TaiKhoan/:MatKhau", async (req, res) => {
   try {
-    const TaiKhoan = req.params.TaiKhoan
-    const MatKhau = req.params.MatKhau
+    const TaiKhoan = req.params.TaiKhoan;
+    const MatKhau = req.params.MatKhau;
     pool.getConnection((error, connection) => {
       if (error) throw error;
-      connection.query(`select * from tbl_nguoi_dung where ten_dang_nhap = N'${TaiKhoan}' and mat_khau = N'${MatKhau}'
-      `, (error, rows) => {
-        connection.release();
-        if (!error) {
-          console.log(rows)
-          res.send(rows);
-          
-        } else {
-          console.log(error);
+      connection.query(
+        `select * from tbl_nguoi_dung where ten_dang_nhap = N'${TaiKhoan}' and mat_khau = N'${MatKhau}'
+      `,
+        (error, rows) => {
+          connection.release();
+          if (!error) {
+            console.log(rows);
+            res.send(rows);
+          } else {
+            console.log(error);
+          }
         }
-      });
+      );
     });
   } catch (error) {
     console.log(error);
@@ -59,213 +61,388 @@ app.get("/DangNhap/:TaiKhoan/:MatKhau", async (req, res) => {
 });
 
 // Kiểm tra tài khoản hệ thống
-app.get('/KiemTraTaiKhoanHeThong/:TaiKhoan' , (req , res)=>{
+app.get("/KiemTraTaiKhoanHeThong/:TaiKhoan", (req, res) => {
   try {
-    const TaiKhoan = req.params.TaiKhoan
+    const TaiKhoan = req.params.TaiKhoan;
     pool.getConnection((error, connection) => {
       if (error) throw error;
-      connection.query(`
+      connection.query(
+        `
         select * from tbl_nguoi_dung where ten_dang_nhap = N'${TaiKhoan}'
-      `, (error, rows) => {
-        connection.release();
-        if (!error) {
-          console.log(rows)
-          res.send(rows);
-        } else {
-          console.log(error);
+      `,
+        (error, rows) => {
+          connection.release();
+          if (!error) {
+            console.log(rows);
+            res.send(rows);
+          } else {
+            console.log(error);
+          }
         }
-      });
+      );
     });
-  } catch (error) {
-    
-  }
-
-})
-app.post(`/DangKyTaiKhoan`,async(req,res)=>{
+  } catch (error) {}
+});
+app.post(`/DangKyTaiKhoan`, async (req, res) => {
   try {
-    const TaiKhoan = req.body.TaiKhoan
-    const MatKhau = req.body.MatKhau
+    const TaiKhoan = req.body.TaiKhoan;
+    const MatKhau = req.body.MatKhau;
     pool.getConnection((error, connection) => {
       if (error) throw error;
-      connection.query("INSERT INTO `tbl_nguoi_dung`( `ten_dang_nhap`, `mat_khau`, `id_nhan_vien`) VALUES (N'"+TaiKhoan+"',N'"+MatKhau+"',1)", (error, rows) => {
-        connection.release();
-        if (!error) {
-          console.log(rows)
-          res.send(rows);
-        } else {
-          console.log(error);
+      connection.query(
+        "INSERT INTO `tbl_nguoi_dung`( `ten_dang_nhap`, `mat_khau`, `id_nhan_vien`) VALUES (N'" +
+          TaiKhoan +
+          "',N'" +
+          MatKhau +
+          "',1)",
+        (error, rows) => {
+          connection.release();
+          if (!error) {
+            console.log(rows);
+            res.send(rows);
+          } else {
+            console.log(error);
+          }
         }
-      });
+      );
     });
-  } catch (error) {
-    
-  }
-})
+  } catch (error) {}
+});
 
 // Đổi mật khẩu
-app.put('/TaiKhoanNguoiDung',(req,res)=>{
+app.put("/TaiKhoanNguoiDung", (req, res) => {
   try {
-    const id_nguoi_dung = req.body.id_nguoi_dung
-    const mat_khau = req.body.mat_khau
+    const id_nguoi_dung = req.body.id_nguoi_dung;
+    const mat_khau = req.body.mat_khau;
     pool.getConnection((error, connection) => {
       if (error) throw error;
-      connection.query("UPDATE `tbl_nguoi_dung` SET `mat_khau`='"+mat_khau+"' WHERE id_nguoi_dung = "+id_nguoi_dung
-      , (error, rows) => {
-        connection.release();
-        if (!error) {
-          console.log(rows)
-          res.send(rows);
-        } else {
-          console.log(error);
+      connection.query(
+        "UPDATE `tbl_nguoi_dung` SET `mat_khau`='" +
+          mat_khau +
+          "' WHERE id_nguoi_dung = " +
+          id_nguoi_dung,
+        (error, rows) => {
+          connection.release();
+          if (!error) {
+            console.log(rows);
+            res.send(rows);
+          } else {
+            console.log(error);
+          }
         }
-      });
+      );
     });
-  } catch (error) {
-    
-  }
-})
-
+  } catch (error) {}
+});
 
 // Lấy thông tin nhân viên
-app.get('/ThongTinNhanVien/:id_nhan_vien' , (req , res)=>{
-   try {
-    const id_nhan_vien = req.params.id_nhan_vien
+app.get("/ThongTinNhanVien/:id_nhan_vien", (req, res) => {
+  try {
+    const id_nhan_vien = req.params.id_nhan_vien;
     pool.getConnection((error, connection) => {
       if (error) throw error;
-      connection.query("select * from tbl_nhan_vien WHERE id_nhan_vien = "+id_nhan_vien, (error, rows) => {
-        connection.release();
-        if (!error) {
-          console.log(rows)
-          res.send(rows);
-        } else {
-          console.log(error);
+      connection.query(
+        "select * from tbl_nhan_vien WHERE id_nhan_vien = " + id_nhan_vien,
+        (error, rows) => {
+          connection.release();
+          if (!error) {
+            console.log(rows);
+            res.send(rows);
+          } else {
+            console.log(error);
+          }
         }
-      });
+      );
     });
-   } catch (error) {
-     
-   }
-
-})
+  } catch (error) {}
+});
 
 //#endregion
 
 //#region (2) Chức năng quản lý tài khoản nhân viên
-app.post('/ThemLoaiNhanVien' , (req , res)=>{
+
+// Thêm loại nhân viên
+app.post("/ThemLoaiNhanVien", (req, res) => {
   try {
     // const id_nhan_vien = req.body.id_nhan_vien
-    const ten_loai_nhan_vien = req.body.ten_loai_nhan_vien
-    const ghi_chu = req.body.ghi_chu
+    const ten_loai_nhan_vien = req.body.ten_loai_nhan_vien;
+    const ghi_chu = req.body.ghi_chu;
     pool.getConnection((error, connection) => {
       if (error) throw error;
-      connection.query(`
+      connection.query(
+        `
         INSERT INTO tbl_loai_nhan_vien(ten_loai_nhan_vien, ghi_chu)
         VALUES (N'${ten_loai_nhan_vien}',N'${ghi_chu}');
-      `, (error, rows) => {
-        connection.release();
-        if (!error) {
-          console.log(rows)
-          res.send(rows);
-        } else {
-          console.log(error);
+      `,
+        (error, rows) => {
+          connection.release();
+          if (!error) {
+            console.log(rows);
+            res.send(rows);
+          } else {
+            console.log(error);
+          }
         }
-      });
+      );
     });
-  } catch (error) {
-    
-  }
-})
-app.get('/DanhSachTaiKhoan' , (req , res)=>{
+  } catch (error) {}
+});
+
+// Danh sách tài khoản
+app.get("/DanhSachTaiKhoan", (req, res) => {
   try {
-    const id_nhan_vien = req.params.id_nhan_vien
+    const id_nhan_vien = req.params.id_nhan_vien;
     pool.getConnection((error, connection) => {
       if (error) throw error;
-      connection.query(`
+      connection.query(
+        `
         select * from tbl_nguoi_dung,tbl_nhan_vien WHERE
         tbl_nguoi_dung.id_nhan_vien = tbl_nhan_vien.id_nhan_vien
-      `, (error, rows) => {
-        connection.release();
-        if (!error) {
-          console.log(rows)
-          res.send(rows);
-        } else {
-          console.log(error);
+      `,
+        (error, rows) => {
+          connection.release();
+          if (!error) {
+            console.log(rows);
+            res.send(rows);
+          } else {
+            console.log(error);
+          }
         }
-      });
+      );
     });
-  } catch (error) {
-    
-  }
-})
+  } catch (error) {}
+});
 
 // Thêm thông tin tài khoản nhân viên
-app.post('/ThemThongTinTaiKhoanNhanVien' , (req , res)=>{
+app.post("/ThemThongTinTaiKhoanNhanVien", (req, res) => {
   try {
-    const id_loai_nhan_vien = req.body.id_loai_nhan_vien
-    const ten_nhan_vien = req.body.ten_nhan_vien
-    const lien_he = req.body.lien_he
-    const noi_quy = req.body.noi_quy
-    const ten_dang_nhap = req.body.ten_dang_nhap
-    const mat_khau = req.body.mat_khau
+    const id_loai_nhan_vien = req.body.id_loai_nhan_vien;
+    const ten_nhan_vien = req.body.ten_nhan_vien;
+    const lien_he = req.body.lien_he;
+    const noi_quy = req.body.noi_quy;
+    const ten_dang_nhap = req.body.ten_dang_nhap;
+    const mat_khau = req.body.mat_khau;
 
     pool.getConnection((error, connection) => {
       if (error) throw error;
-      connection.query(`
+      connection.query(
+        `
         INSERT INTO tbl_nhan_vien(id_loai_nhan_vien, ten_nhan_vien, lien_he, noi_quy) 
         VALUES (${id_loai_nhan_vien},N'${ten_nhan_vien}','${lien_he}','${noi_quy}');
         INSERT INTO tbl_nguoi_dung(ten_dang_nhap, mat_khau, id_nhan_vien) 
         VALUES ('${ten_dang_nhap}','${mat_khau}', ( select id_nhan_vien from tbl_nhan_vien where ten_nhan_vien = N'${ten_nhan_vien}' ) );
         
-      `, (error, rows) => {
-        connection.release();
-        if (!error) {
-          console.log(rows)
-          res.send(rows);
-        } else {
-          console.log(error);
+      `,
+        (error, rows) => {
+          connection.release();
+          if (!error) {
+            console.log(rows);
+            res.send(rows);
+          } else {
+            console.log(error);
+          }
         }
-      });
+      );
     });
-  } catch (error) {
-    
-  }
-})
-
-// Mở/Đóng tài khoản truy cập App
-app.get('/MoDongTaiKhoanTruyCapApp/:id_nguoi_dung/:trang_thai' , (req , res)=>{
+  } catch (error) {}
+});
+// Sửa
+app.put("/SuaThongTinTaiKhoanNhanVien", (req, res) => {
   try {
-    const id_nguoi_dung = req.params.id_nguoi_dung
-    const trang_thai = req.params.trang_thai === true ? '1' : '0'
+    const id_loai_nhan_vien = req.body.id_loai_nhan_vien;
+    const id_nhan_vien = req.body.id_nhan_vien;
+    const ten_nhan_vien = req.body.ten_nhan_vien;
+    const lien_he = req.body.lien_he;
+    const noi_quy = req.body.noi_quy;
+    const ten_dang_nhap = req.body.ten_dang_nhap;
+    const mat_khau = req.body.mat_khau;
+    const id_nguoi_dung = req.body.id_nguoi_dung;
     pool.getConnection((error, connection) => {
       if (error) throw error;
-      connection.query(`
-        update tbl_nguoi_dung set trang_thai = ${trang_thai} where id_nguoi_dung = ${id_nguoi_dung}
-      `, (error, rows) => {
-        connection.release();
-        if (!error) {
-          console.log(rows)
-          res.send(rows);
-        } else {
-          console.log(error);
+      connection.query(
+        `
+        update tbl_nhan_vien SET
+        ten_nhan_vien = N'${ten_nhan_vien}',lien_he = N'${lien_he}',noi_quy=N'${noi_quy}' where id_nhan_vien = ${id_nhan_vien};
+        update tbl_nguoi_dung set ten_dang_nhap = N'${ten_dang_nhap}',mat_khau=N'${mat_khau}'
+        where id_nguoi_dung= ${id_nguoi_dung}
+      `,
+        (error, rows) => {
+          connection.release();
+          if (!error) {
+            console.log(rows);
+            res.send(rows);
+          } else {
+            console.log(error);
+          }
         }
-      });
+      );
     });
-  } catch (error) {
-    
-  }
-})
+  } catch (error) {}
+});
+// Xoá tài khoản người dùng
+app.delete("/XoaThongTinTaiKhoanNhanVien", (req, res) => {
+  try {
+    // const id_loai_nhan_vien = req.body.id_loai_nhan_vien;
+    // const id_nhan_vien = req.body.id_nhan_vien;
+    // const ten_nhan_vien = req.body.ten_nhan_vien;
+    // const lien_he = req.body.lien_he;
+    // const noi_quy = req.body.noi_quy;
+    // const ten_dang_nhap = req.body.ten_dang_nhap;
+    // const mat_khau = req.body.mat_khau;
+    const id_nguoi_dung = req.body.id_nguoi_dung;
+    pool.getConnection((error, connection) => {
+      if (error) throw error;
+      connection.query(
+        `
+          delete from tbl_nguoi_dung where id_nguoi_dung = ${id_nguoi_dung}
+      `,
+        (error, rows) => {
+          connection.release();
+          if (!error) {
+            console.log(rows);
+            res.send(rows);
+          } else {
+            console.log(error);
+          }
+        }
+      );
+    });
+  } catch (error) {}
+});
+
+// Mở/Đóng tài khoản truy cập App
+app.get("/MoDongTaiKhoanTruyCapApp/:id_nguoi_dung/:trang_thai", (req, res) => {
+  try {
+    const id_nguoi_dung = req.params.id_nguoi_dung;
+    const trang_thai = req.params.trang_thai === true ? "1" : "0";
+    pool.getConnection((error, connection) => {
+      if (error) throw error;
+      connection.query(
+        `
+        update tbl_nguoi_dung set trang_thai = ${trang_thai} where id_nguoi_dung = ${id_nguoi_dung}
+      `,
+        (error, rows) => {
+          connection.release();
+          if (!error) {
+            console.log(rows);
+            res.send(rows);
+          } else {
+            console.log(error);
+          }
+        }
+      );
+    });
+  } catch (error) {}
+});
 //#endregion
 
 //#region (3) Quản lý chấm công
 
+// Thêm giờ công nhân viên
+app.get("/ThemGioCongNhanVien", async (req, res) => {
+  try {
+    const id_nguoi_dung = req.params.id_nguoi_dung;
+
+    const id_cong_nhan = req.params.id_cong_nhan;
+    const ngay_thuc_hien = req.params.ngay_thuc_hien;
+    const gio_vao_cong = req.params.gio_vao_cong; // 2020-03-27 08:00
+    const gio_ra_cong = req.params.gio_ra_cong;
+    pool.getConnection((error, connection) => {
+      if (error) throw error;
+      const dl = [];
+      console.log(dl);
+      connection.query(
+        `
+          INSERT INTO tbl_cong_nhan_vien(
+              id_cong_nhan,ngay_thuc_hien
+          )
+          values(${id_cong_nhan},'${ngay_thuc_hien}');
+            
+          INSERT INTO tbl_cong_nhan_vien_chi_tiet(
+            gio_vao_cong, gio_ra_cong, ngay_lam_viec, id_cong_nhan_vien) 
+            VALUES(
+            '${gio_vao_cong}',
+            '${gio_ra_cong}',
+            '${ngay_thuc_hien}',
+               (SELECT id_cong_nhan_vien FROM tbl_cong_nhan_vien WHERE id_cong_nhan = ${id_cong_nhan} and ngay_thuc_hien = '${ngay_thuc_hien}')
+          )
+
+        `,
+        (error, rows) => {
+          connection.release();
+          if (!error) {
+            console.log(rows);
+            res.send(rows);
+          } else {
+            console.log(error);
+          }
+        }
+      );
+    });
+  } catch (error) {}
+});
+
+// Hiển thị công theo ngày
+app.put("/HienThiCongTheoNgay", async (req, res) => {
+  try {
+    const id_cong_nhan = req.params.id_nguoi_dung;
+    const ngay_thuc_hien = req.params.ngay_thuc_hien;
+    const trang_thai_vao_ra = req.params.trang_thai_vao_ra === 1 ? true : false;
+    const gio_vao_thuc_te = req.params.gio_vao_thuc_te;
+    const gio_ra_thuc_te = req.params.gio_ra_thuc_te;
+    const ngay_thuc_hien = req.params.ngay_thuc_hien;
+    pool.getConnection((error, connection) => {
+      if (error) throw error;
+      connection.query(
+        trang_thai_vao_ra
+          ? `
+        update tbl_cong_nhan_vien_chi_tiet set gio_vao_thuc_te = '${gio_vao_thuc_te}' WHERE id_cong_nhan_vien_chi_tiet = 
+        (
+        select tbl_cong_nhan_vien_chi_tiet.id_cong_nhan_vien_chi_tiet from tbl_cong_nhan_vien_chi_tiet,tbl_cong_nhan_vien
+        WHERE tbl_cong_nhan_vien_chi_tiet.id_cong_nhan_vien = tbl_cong_nhan_vien.id_cong_nhan_vien
+        and tbl_cong_nhan_vien.id_cong_nhan = ${id_cong_nhan} and
+        tbl_cong_nhan_vien.ngay_thuc_hien = '${ngay_thuc_hien}'
+        )
+        `
+          : `
+        update tbl_cong_nhan_vien_chi_tiet set gio_ra_thuc_te = '${gio_ra_thuc_te}' WHERE id_cong_nhan_vien_chi_tiet = 
+        (
+        select tbl_cong_nhan_vien_chi_tiet.id_cong_nhan_vien_chi_tiet from tbl_cong_nhan_vien_chi_tiet,tbl_cong_nhan_vien
+        WHERE tbl_cong_nhan_vien_chi_tiet.id_cong_nhan_vien = tbl_cong_nhan_vien.id_cong_nhan_vien
+        and tbl_cong_nhan_vien.id_cong_nhan = ${id_cong_nhan} and
+        tbl_cong_nhan_vien.ngay_thuc_hien = '${ngay_thuc_hien}'
+        )
+        `,
+        (error, rows) => {
+          connection.release();
+          if (!error) {
+            console.log(rows);
+            res.send(rows);
+          } else {
+            console.log(error);
+          }
+        }
+      );
+    });
+  } catch (error) {}
+});
+
+// Chấm vào công nhân
+
+// Chấm ra công nhân
 //#endregion
 
-//#region (3) Quản lý chấm công
-
+//#region (5) Quản lý dự án
 //#endregion
 
-//
+//#region (6) Chức năng thông báo
+//#endregion
 
+//#region (8) Chức năng quản lý điều khoản chính sách
+//#endregion
+
+//#region (10) Chức năng quản lý điều khoản chính sách
+//#endregion
 
 // ************************************************************
 console.log("test");
